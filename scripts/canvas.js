bd3db8cd42d0eca949fc94c6a8c7ea1299598507
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-07-03 22:58:35 +0530

    basic whiteboard added

---
+++ scripts/canvas.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', ()=> {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    let painting =false;
+    function startPosition(){
+        painting=true;
+        ctx.beginPath();
+    }
+    function finishedPosition(){
+        painting=false;
+    }
+    function draw(event){
+        if(!painting) return;
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = 'blue';
+        ctx.lineCap = 'round';
+        ctx.lineTo(event.clientX, event.clientY);
+        ctx.stroke();
+    }
+    function erase(event){
+        if(!painting) return;
+        ctx.lineWidth = 30;
+        ctx.strokeStyle = 'white';
+        ctx.lineCap = 'round';
+        ctx.lineTo(event.clientX, event.clientY);
+        ctx.stroke();
+    }
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', finishedPosition);
+    canvas.addEventListener('mousemove', draw);
+})
