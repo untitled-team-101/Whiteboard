c3e680c41c7678551959e997a42d3432ef565a57
azhar <ahmedazhar110011@gmail.com>
2021-07-04 22:03:33 +0530

    for dhruv

--- scripts/multiboard.js
+++ scripts/multiboard.js
@@ -3,8 +3,9 @@ let prev = document.getElementById('prev')
 let next = document.getElementById('next')
 let add = document.getElementById('add')
 
-let boardId = 0
-    // setInterval(currentBoard, 10)
+// let boardId = 0
+let currentIndex = -1;
+// setInterval(currentBoard, 10)
 
 // function currentBoard() {
 //     let curr = canvas.toDataURL()
@@ -13,11 +14,9 @@ let boardId = 0
 //     boardSection.appendChild()
 // }
 
-console.log(canvas);
 let boardStore = []
-let boardTemplate = function(boardNum, boardState, board, boardUndoList, boardRedoList) {
+let boardTemplate = function(boardState, board, boardUndoList, boardRedoList) {
     let boardData = {
-        boardNum,
         boardState,
         board,
         boardUndoList,
@@ -26,20 +25,83 @@ let boardTemplate = function(boardNum, boardState, board, boardUndoList, boardRe
     return boardData
 }
 
+let last = 0;
+
 function addBoard() {
-    boardId += 1
+    last = Math.max(last, currentIndex);
+    currentIndex += 1;
     let board = canvas.toDataURL()
-    let currentBoard = boardTemplate(boardId, false, board, save.undo_list, save.redo_list)
+    let currentBoard = boardTemplate(false, board, save.undo_list, save.redo_list)
     save.undo_list = []
     save.redo_list = []
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    let img = document.createElement('img')
-    img.src = board;
-    img.height = "100"
-    img.width = "100"
-    img.style.backgroundColor = "red"
-    boardSection.appendChild(img)
+        // let img = document.createElement('img')
+        // let div = document.createElement('div')
+        // img.src = board;
+        // img.height = "100"
+        // img.width = "100"
+        // img.style.backgroundColor = "red"
+        // div.appendChild(img)
+        // div.classList.add('board')
+        // boardSection.appendChild(div)
     boardStore.push(currentBoard)
-    console.log(boardStore)
+    showPreview();
 }
+
+function prevBoard() {
+    console.log(currentIndex, boardStore.length)
+    if (currentIndex === 0) {
+        window.alert("ANDHE PEECHE KOI BOARD NAHI HAI !!")
+    } else {
+        if (currentIndex === boardStore.length - 1) {
+            addBoard()
+        }
+        currentIndex -= 1
+        boardStore[currentIndex + 1].board = canvas.toDataURL()
+        console.log(boardStore[currentIndex + 1].board)
+        let previousState = boardStore[currentIndex].board
+        let currImg = document.createElement('img')
+        currImg.src = previousState
+        currImg.onload = function() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            let div = document.createElement('div')
+            img.src = board;
+            img.height = "100"
+            ctx.drawImage(currImg, 0, 0)
+        }
+    }
+    showPreview();
+}
+
+function nextBoard() {
+    if (currentIndex >= boardStore.length - 1) {
+        window.alert("ANDHE AAGE KOI BOARD NAHI HAI !!")
+    } else {
+        currentIndex += 1;
+        boardStore[currentIndex - 1].board = canvas.toDataURL()
+        let nextState = boardStore[currentIndex].board
+        let currImg = document.createElement('img')
+        currImg.src = nextState
+        currImg.onload = function() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.drawImage(currImg, 0, 0)
+        }
+    }
+    showPreview();
+}
+
+function showPreview() {
+    boardSection.innerHTML = "";
+    for (let boardPreview of boardStore) {
+        let previewImg = document.createElement('img');
+        previewImg.src = boardPreview.board;
+        previewImg.height = "100";
+        previewImg.width = "100";
+        previewImg.style.backgroundColor = "yellow";
+        boardSection.appendChild(previewImg);
+    }
+}
+
 add.addEventListener("click", addBoard)
+prev.addEventListener("click", prevBoard)
+next.addEventListener("click", nextBoard)
