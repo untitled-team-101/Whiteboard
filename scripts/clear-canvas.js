0752e79eca2b93bbec55164e9f4fc2b04c3f9b89
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-07-05 18:01:03 +0530

    minor bugs resolves

--- scripts/clear-canvas.js
+++ scripts/clear-canvas.js
@@ -1,5 +1,14 @@
 let clear = document.getElementById("clear-canvasPenbar");
+let cleardef = document.querySelector('#clear-canvasDefbar');
+
 clear.addEventListener("click", () => {
+    save.saveUndoState();
+    ctx.fillStyle = pencilBoxVars.backgroundColor;
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+})
+cleardef.addEventListener("click", () => {
+    save.saveUndoState();
     ctx.fillStyle = pencilBoxVars.backgroundColor;
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.fillRect(0,0, canvas.width, canvas.height);
