4e61f33b08aad935479fb3dceb449fa4b442c612
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-07-05 22:03:04 +0530

    added text color and text size to add text

--- scripts/addText.js
+++ scripts/addText.js
@@ -10,11 +10,31 @@ addTextToolButton.addEventListener("click", ()=>{
     addTextField.value = ""
 })
 
+let addTextVars = {
+    textSize: 18,
+    textColor: "#ffffff"
+}
+
+document.getElementById("text-color-picker")
+    .addEventListener("input", (event)=>{
+        console.log("input2")
+        addTextVars.textColor = event.target.value
+        addTextField.style.color = event.target.value
+    })
+
+document.getElementById("text-stroke-width")
+    .addEventListener("input", (event)=>{
+        console.log("input")
+        addTextVars.textSize = event.target.value
+        addTextField.style.fontSize = event.target.value + 'px'
+    })
+
 function addTextToCanvas(){
     let text = addTextField.value
     function addText(event) {
         save.saveUndoState();
-        ctx.font = "40px 'Montserrat'";
+        ctx.font = `${addTextVars.textSize}px 'serif'`;
+        ctx.fillStyle = addTextVars.textColor
         ctx.fillText(text, event.clientX, event.clientY)
         canvas.removeEventListener("click", addText)
     }
