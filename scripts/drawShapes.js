e449a9f5b2a1fda9e23a34c24b4093b71c71c0ff
azhar <ahmedazhar110011@gmail.com>
2021-07-05 15:41:18 +0530

    undo-redo functionlity added to draw tool sand shapes

--- scripts/drawShapes.js
+++ scripts/drawShapes.js
@@ -7,7 +7,7 @@ let shapeDrawVars = {
     prevCtx: null
 }
 
-function setLineStart(event){
+function setLineStart(event) {
     painting = true
     shapeDrawVars.startX = event.clientX
     shapeDrawVars.startY = event.clientY
@@ -18,28 +18,29 @@ function setLineStart(event){
     ctx.moveTo(event.clientX, event.clientY)
     ctx.lineTo(event.clientX, event.clientY)
     ctx.stroke()
+    save.saveUndoState();
 }
 
-function showCurrentLine(event){
-    if(!painting)
+function showCurrentLine(event) {
+    if (!painting)
         return
     ctx.beginPath()
     let image = document.createElement('img')
     image.src = shapeDrawVars.prevCtx;
-    image.onload = function(){
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-        ctx.drawImage(image,0,0,canvas.width,canvas.height)
+    image.onload = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
         ctx.moveTo(shapeDrawVars.startX, shapeDrawVars.startY)
         ctx.lineTo(event.clientX, event.clientY);
         ctx.stroke();
     }
 }
 
-function drawLine(event){
+function drawLine(event) {
     painting = false;
 }
 
-function setRectStart(event){
+function setRectStart(event) {
     painting = true
     shapeDrawVars.startX = event.clientX
     shapeDrawVars.startY = event.clientY
@@ -50,28 +51,29 @@ function setRectStart(event){
     ctx.moveTo(event.clientX, event.clientY)
     ctx.rect(shapeDrawVars.startX, shapeDrawVars.startY, event.clientX - shapeDrawVars.startX, event.clientY - shapeDrawVars.startY)
     ctx.stroke()
+    save.saveUndoState();
 }
 
-function showCurrentRect(event){
-    if(!painting)
+function showCurrentRect(event) {
+    if (!painting)
         return
     ctx.beginPath()
     let image = document.createElement('img')
     image.src = shapeDrawVars.prevCtx;
-    image.onload = function(){
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-        ctx.drawImage(image,0,0,canvas.width,canvas.height)
+    image.onload = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
         ctx.moveTo(shapeDrawVars.startX, shapeDrawVars.startY)
         ctx.rect(shapeDrawVars.startX, shapeDrawVars.startY, event.clientX - shapeDrawVars.startX, event.clientY - shapeDrawVars.startY)
         ctx.stroke();
     }
 }
 
-function drawRect(event){
+function drawRect(event) {
     painting = false;
 }
 
-function setCircleStart(event){
+function setCircleStart(event) {
     painting = true
     shapeDrawVars.startX = event.clientX
     shapeDrawVars.startY = event.clientY
@@ -80,26 +82,27 @@ function setCircleStart(event){
     ctx.lineWidth = shapeDrawVars.strokeWidth
     ctx.strokeStyle = shapeDrawVars.strokeColor
     let radius = Math.sqrt(
-        Math.pow(shapeDrawVars.startX - event.clientX, 2)
-        + Math.pow(shapeDrawVars.startY - event.clientY, 2)
+        Math.pow(shapeDrawVars.startX - event.clientX, 2) +
+        Math.pow(shapeDrawVars.startY - event.clientY, 2)
     )
     ctx.moveTo(length + shapeDrawVars.startX, shapeDrawVars.startY)
     ctx.arc(shapeDrawVars.startX, shapeDrawVars.startY, radius, 0, 2 * Math.PI)
     ctx.stroke()
+    save.saveUndoState();
 }
 
-function showCurrentCircle(event){
-    if(!painting)
+function showCurrentCircle(event) {
+    if (!painting)
         return
     ctx.beginPath()
     let image = document.createElement('img')
     image.src = shapeDrawVars.prevCtx;
-    image.onload = function(){
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-        ctx.drawImage(image,0,0,canvas.width,canvas.height)
+    image.onload = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
         let radius = Math.sqrt(
-            Math.pow(shapeDrawVars.startX - event.clientX, 2)
-            + Math.pow(shapeDrawVars.startY - event.clientY, 2)
+            Math.pow(shapeDrawVars.startX - event.clientX, 2) +
+            Math.pow(shapeDrawVars.startY - event.clientY, 2)
         )
         ctx.moveTo(radius + shapeDrawVars.startX, shapeDrawVars.startY)
         ctx.arc(shapeDrawVars.startX, shapeDrawVars.startY, radius, 0, 2 * Math.PI)
@@ -107,11 +110,11 @@ function showCurrentCircle(event){
     }
 }
 
-function drawCircle(event){
+function drawCircle(event) {
     painting = false;
 }
 
-function setTriangleStart(event){
+function setTriangleStart(event) {
     painting = true
     shapeDrawVars.startX = event.clientX
     shapeDrawVars.startY = event.clientY
@@ -119,20 +122,21 @@ function setTriangleStart(event){
     ctx.beginPath()
     ctx.lineWidth = shapeDrawVars.strokeWidth
     ctx.strokeStyle = shapeDrawVars.strokeColor
+    save.saveUndoState();
 }
 
-function showCurrentTriangle(event){
-    if(!painting)
+function showCurrentTriangle(event) {
+    if (!painting)
         return
     let image = document.createElement('img')
     image.src = shapeDrawVars.prevCtx;
-    image.onload = function(){
+    image.onload = function() {
         ctx.beginPath()
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-        ctx.drawImage(image,0,0,canvas.width,canvas.height)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
         let radius = Math.sqrt(
-            Math.pow(shapeDrawVars.startX - event.clientX, 2)
-            + Math.pow(shapeDrawVars.startY - event.clientY, 2)
+            Math.pow(shapeDrawVars.startX - event.clientX, 2) +
+            Math.pow(shapeDrawVars.startY - event.clientY, 2)
         )
         ctx.moveTo((shapeDrawVars.startX + event.clientX) / 2, shapeDrawVars.startY)
         ctx.lineTo(event.clientX, event.clientY)
@@ -142,36 +146,33 @@ function showCurrentTriangle(event){
     }
 }
 
-function drawTriangle(event){
+function drawTriangle(event) {
     painting = false;
 }
 
-for(let addShapeButton of addShapeButtons){
-    addShapeButton.addEventListener("click", (event)=>{
+for (let addShapeButton of addShapeButtons) {
+    addShapeButton.addEventListener("click", (event) => {
         console.log(addShapeButton.id)
         removeEvents()
-        if(addShapeButton.id === "shape-line"){
+        if (addShapeButton.id === "shape-line") {
             onMouseDownEvent = setLineStart
             onMouseUpEvent = drawLine
-            onMouseMoveEvent =  showCurrentLine
+            onMouseMoveEvent = showCurrentLine
             onMouseLeaveEvent = drawLine
-        }
-        else if(addShapeButton.id === "shape-rect"){
+        } else if (addShapeButton.id === "shape-rect") {
             onMouseDownEvent = setRectStart
             onMouseUpEvent = drawRect
-            onMouseMoveEvent =  showCurrentRect
+            onMouseMoveEvent = showCurrentRect
             onMouseLeaveEvent = drawRect
-        }
-        else if(addShapeButton.id === "shape-circle"){
+        } else if (addShapeButton.id === "shape-circle") {
             onMouseDownEvent = setCircleStart
             onMouseUpEvent = drawCircle
-            onMouseMoveEvent =  showCurrentCircle
+            onMouseMoveEvent = showCurrentCircle
             onMouseLeaveEvent = drawCircle
-        }
-        else if(addShapeButton.id === "shape-triangle"){
+        } else if (addShapeButton.id === "shape-triangle") {
             onMouseDownEvent = setTriangleStart
             onMouseUpEvent = drawTriangle
-            onMouseMoveEvent =  showCurrentTriangle
+            onMouseMoveEvent = showCurrentTriangle
             onMouseLeaveEvent = drawTriangle
         }
         addAllEvents()
--- scripts/pencilBox.js
+++ scripts/pencilBox.js
@@ -2,111 +2,113 @@
 let penBoxEle = document.querySelectorAll('.penbarEle');
 
 let pencilBoxVars = {
-    backgroundColor : '#333333',
-    paintingColorOne : 'red',
-    paintingColorTwo : 'blue',
-    highlighterColor : '#00FF0004',
-    painting : false,
+    backgroundColor: '#333333',
+    paintingColorOne: 'red',
+    paintingColorTwo: 'blue',
+    highlighterColor: '#00FF0004',
+    painting: false,
     penWidth1: 10,
     penWidth2: 10,
-    eraserWidth:50
+    eraserWidth: 50
 };
 
 
 
-function addPenOneEvents(){
+function addPenOneEvents() {
     removeEvents();
-    onMouseMoveEvent = function (event){
-        if(!pencilBoxVars.painting) return;
+    onMouseMoveEvent = function(event) {
+        if (!pencilBoxVars.painting) return;
         ctx.lineWidth = pencilBoxVars.penWidth1;
         ctx.strokeStyle = pencilBoxVars.paintingColorOne;
         ctx.lineCap = 'round';
         ctx.lineTo(event.clientX, event.clientY);
         ctx.stroke();
     }
-    onMouseDownEvent = function (){
-        pencilBoxVars.painting=true;
+    onMouseDownEvent = function() {
+        pencilBoxVars.painting = true;
         ctx.beginPath();
+        save.saveUndoState();
     }
-    onMouseUpEvent = function () {
+    onMouseUpEvent = function() {
         pencilBoxVars.painting = false;
     }
     addAllEvents();
 }
-function addPenTwoEvents(){
+
+function addPenTwoEvents() {
     removeEvents();
-    onMouseMoveEvent = function (event){
-        if(!pencilBoxVars.painting) return;
+    onMouseMoveEvent = function(event) {
+        if (!pencilBoxVars.painting) return;
         ctx.lineWidth = pencilBoxVars.penWidth2;
         ctx.strokeStyle = pencilBoxVars.paintingColorTwo;
         ctx.lineCap = 'round';
         ctx.lineTo(event.clientX, event.clientY);
         ctx.stroke();
     }
-    onMouseDownEvent = function (){
-        pencilBoxVars.painting=true;
+    onMouseDownEvent = function() {
+        pencilBoxVars.painting = true;
         ctx.beginPath();
+        save.saveUndoState();
     }
-    onMouseUpEvent = function () {
+    onMouseUpEvent = function() {
         pencilBoxVars.painting = false;
     }
     addAllEvents();
 }
 
-function addHighlighterEvents(){
+function addHighlighterEvents() {
     removeEvents();
-    onMouseMoveEvent = function (event){
-        if(!pencilBoxVars.painting) return;
+    onMouseMoveEvent = function(event) {
+        if (!pencilBoxVars.painting) return;
         ctx.lineWidth = 30;
         ctx.strokeStyle = 'greenyellow';
         ctx.lineCap = 'square';
         ctx.lineTo(event.clientX, event.clientY);
         ctx.stroke();
     }
-    onMouseDownEvent = function (){
-        pencilBoxVars.painting=true;
+    onMouseDownEvent = function() {
+        pencilBoxVars.painting = true;
         ctx.beginPath();
+        save.saveUndoState();
     }
-    onMouseUpEvent = function () {
+    onMouseUpEvent = function() {
         pencilBoxVars.painting = false;
     }
     addAllEvents();
 }
 
 
-function addEraserEvents(){
+function addEraserEvents() {
     removeEvents();
-    onMouseMoveEvent = function (event){
-        if(!pencilBoxVars.painting) return;
+    onMouseMoveEvent = function(event) {
+        if (!pencilBoxVars.painting) return;
         ctx.lineWidth = pencilBoxVars.eraserWidth;
         ctx.strokeStyle = pencilBoxVars.backgroundColor;
         ctx.lineCap = 'round';
         ctx.lineTo(event.clientX, event.clientY);
         ctx.stroke();
     }
-    onMouseDownEvent = function (){
-        pencilBoxVars.painting=true;
+    onMouseDownEvent = function() {
+        pencilBoxVars.painting = true;
         ctx.beginPath();
+        save.saveUndoState();
     }
-    onMouseUpEvent = function () {
+    onMouseUpEvent = function() {
         pencilBoxVars.painting = false;
     }
     addAllEvents();
 }
 
-for(let ele of penBoxEle) {
-    ele.addEventListener('click', function () {
+for (let ele of penBoxEle) {
+    ele.addEventListener('click', function() {
         console.log(ele.id)
-        if(ele.id === 'penOne'){
+        if (ele.id === 'penOne') {
             addPenOneEvents();
-        }
-        else if(ele.id === 'penTwo'){
+        } else if (ele.id === 'penTwo') {
             addPenTwoEvents();
-        }
-        else if(ele.id === 'eraser') {
+        } else if (ele.id === 'eraser') {
             addEraserEvents();
-        }
-        else if(ele.id === 'highlighter'){
+        } else if (ele.id === 'highlighter') {
             addHighlighterEvents();
         }
     });
